24aaefc267148939f86fc929e6efa7117a4c46aa
Maximpukhov <124022126+Maximpukhov@users.noreply.github.com>
2023-05-13 18:32:17 +0700

    Update command.c

--- lab3/src/command.c
+++ lab3/src/command.c
@@ -1,54 +1,59 @@
 #include <stdio.h>
+
 #include <inttypes.h>
+
 #include "command.h"
+
 #include "coder.h"
-int encode_file(const char *in_file_name, const char *out_file_name)
-{
-    FILE *in;
-    FILE *out;
+
+int encode_file(const char * in_file_name,
+    const char * out_file_name) {
+    FILE * in;
+    FILE * out;
 
     in = fopen(in_file_name, "r");
 
-    if (!in)
-    {
+    if (!in) {
         return -1;
     }
 
     out = fopen(out_file_name, "wb");
 
-    if (!out)
-    {
+    if (!out) {
+        fclose(in);
         return -1;
     }
-    while (!feof(in))
-    {
-        uint32_t code_point;
-        CodeUnit code_unit;
-        fscanf(in, "%" SCNx32, &code_point);
-        if (encode(code_point, &code_unit) < 0)
-        {
+
+    uint32_t code_point;
+    CodeUnit code_unit;
+
+    while (fscanf(in, "%"
+            SCNx32, & code_point) == 1) {
+        if (encode(code_point, & code_unit) < 0) {
             printf("Error encode function\n");
+            fclose(in);
+            fclose(out);
             return -1;
         }
-        write_code_unit(out, &code_unit);
+
+        write_code_unit(out, & code_unit);
     }
+
     fclose(in);
     fclose(out);
     return 0;
 }
 
-int decode_file(const char *in_file_name, const char *out_file_name)
-{
-    FILE *in;
-    FILE *out;
+int decode_file(const char * in_file_name,
+    const char * out_file_name) {
+    FILE * in;
+    FILE * out;
     in = fopen(in_file_name, "r");
-    if (!in)
-    {
+    if (!in) {
         return -1;
     }
     out = fopen(out_file_name, "wb");
-    if (!out)
-    {
+    if (!out) {
         return -1;
     }
     CodeUnit code_unit;
@@ -56,17 +61,15 @@ int decode_file(const char *in_file_name, const char *out_file_name)
     fseek(in, 0, SEEK_END);
     long end_symbol = ftell(in);
     fseek(in, 0, SEEK_SET);
-    while ((ftell(in)) != (end_symbol))
-    {
-        if ((read_next_code_unit(in, &code_unit)) == 0)
-        {
-            uint32_t num = decode(&code_unit);
-            if (decode(&code_unit) < 0)
-            {
+    while ((ftell(in)) != (end_symbol)) {
+        if ((read_next_code_unit(in, & code_unit)) == 0) {
+            uint32_t num = decode( & code_unit);
+            if (decode( & code_unit) < 0) {
                 printf("Error decode function\n");
                 return -1;
             }
-            fprintf(out, "%" PRIx32 "\n", num);
+            fprintf(out, "%"
+                PRIx32 "\n", num);
         }
     }
     fclose(in);
