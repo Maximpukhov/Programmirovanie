4e7c3285ba71fd3ed31b0fa6faae20179eb87d44
Maximpukhov <124022126+Maximpukhov@users.noreply.github.com>
2023-05-21 12:19:41 +0700

    Update palindrome.h

--- course work/src/palindrome.h
+++ course work/src/palindrome.h
@@ -1,7 +1,7 @@
 #ifndef PALINDROME_H
 #define PALINDROME_H
 
-Int isPalindrome(const wchar_t* str);
-Void checkPalindromesFromFile(const char* filename);
+int isPalindrome(const wchar_t* str);
+void checkPalindromesFromFile(const char* filename);
 
 #endif /* PALINDROME_H */
