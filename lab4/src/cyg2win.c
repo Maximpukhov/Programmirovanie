1a2e4c8f66880b19211a6a0510457dbc004e4f43
Maximpukhov <124022126+Maximpukhov@users.noreply.github.com>
2023-05-21 16:14:09 +0700

    Update cyg2win.c

--- lab4/src/cyg2win.c
+++ lab4/src/cyg2win.c
@@ -1,10 +1,5 @@
 #include <cyg2win.h>
 
-// maxlen - сколько символов может быть
-// не забыть про:
-// - признак конца строки
-// - канарейку от переполнения
-// - грязный хак для удаления переноса строки
 int input(char* str, int maxlen){
 	fgets(str, maxlen+3, stdin); //
 	if (slen(str) >= maxlen+2)
@@ -13,19 +8,13 @@ int input(char* str, int maxlen){
 	return 0;
 }
 
-// returns index+1by of the  first invalid symbol
-
-// this is super broken
 int check(char* delim, char* paths){
-	// Do both strings even exist?
 	if (delim == NULL || paths == NULL) return -1;
 	char* ptr[MAX_PATH_LEN];
 	int errsym_index;
-	// Are there any individual paths longer than 260 symbols?
 	int cnt = stok(paths, *delim, ptr);
 	for (int i = 0; i < cnt; i++) {
 		if (slen(ptr[i]) > MAX_PATH_LEN) return i+1;
-		// Does it contain forbidden symbols?
 		int forbidden_pos = sspn(ptr[i], FORBIDDEN_SYMBOLS);
 		if (forbidden_pos) return -(errsym_index+forbidden_pos);
 		errsym_index += slen(ptr[i]);
