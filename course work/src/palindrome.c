67e8bf66efbd55a412f7c8c557faa8a785fb81c0
Maximpukhov <124022126+Maximpukhov@users.noreply.github.com>
2023-05-21 12:19:23 +0700

    Update palindrome.c

--- course work/src/palindrome.c
+++ course work/src/palindrome.c
@@ -2,82 +2,82 @@
 #include <wchar.h>
 #include <wctype.h>
 #include <stdlib.h>
-#include «palindrome.h»
+#include "palindrome.h"
 
-Int isPalindrome(const wchar_t* str) {
-    Int length = wcslen(str);
-    Int i = 0;
-    Int j = length – 1;
+int isPalindrome(const wchar_t* str) {
+    int length = wcslen(str);
+    int i = 0;
+    int j = length - 1;
 
-    While (i < j) {
+    while (i < j) {
         // Пропускаем пробелы и знаки пунктуации в начале строки
-        While (i < length && !iswalpha(str[i])) {
-            I++;
+        while (i < length && !iswalpha(str[i])) {
+            i++;
         }
         // Пропускаем пробелы и знаки пунктуации в конце строки
-        While (j >= 0 && !iswalpha(str[j])) {
+        while (j >= 0 && !iswalpha(str[j])) {
             j--;
         }
 
-        If (i < j && towlower(str[i]) != towlower(str[j])) {
-            Return 0; // Не палиндром
+        if (i < j && towlower(str[i]) != towlower(str[j])) {
+            return 0; // Не палиндром
         }
 
-        I++;
+        i++;
         j--;
     }
 
-    Return 1; // Палиндром
+    return 1; // Палиндром
 }
 
-Void checkPalindromesFromFile(const char* filename) {
-    FILE* file = fopen(filename, «r»);
-    If (file == NULL) {
-        Wprintf(L»Не удалось открыть файл.\n»);
-        Return;
+void checkPalindromesFromFile(const char* filename) {
+    FILE* file = fopen(filename, "r");
+    if (file == NULL) {
+        wprintf(L"Не удалось открыть файл.\n");
+        return;
     }
-    Wchar_t* line = malloc(1000 * sizeof(wchar_t)); // Динамическое выделение памяти
-    If (line == NULL) {
-        Wprintf(L»Ошибка выделения памяти.\n»);
-        Fclose(file);
-        Return;
+    wchar_t* line = malloc(1000 * sizeof(wchar_t)); // Динамическое выделение памяти
+    if (line == NULL) {
+        wprintf(L"Ошибка выделения памяти.\n");
+        fclose(file);
+        return;
     }
 
-    Int totalPalindromes = 0; // Общее количество палиндромических предложений
-    Wprintf(L»Палиндромические предложения:\n»);
+    int totalPalindromes = 0; // Общее количество палиндромических предложений
+    wprintf(L"Палиндромические предложения:\n");
 
-    While (fgetws(line, 1000, file)) {
+    while (fgetws(line, 1000, file)) {
         // Удаление символа новой строки, если присутствует
-        If (line[wcslen(line) – 1] == L'\n') {
-            Line[wcslen(line) – 1] = L'\0';
+        if (line[wcslen(line) - 1] == L'\n') {
+            line[wcslen(line) - 1] = L'\0';
         }
 
-        Wchar_t* context;
-        Wchar_t* sentence = wcstok(line, L».!?\n», &context); // Разделение строки на предложения
+        wchar_t* context;
+        wchar_t* sentence = wcstok(line, L".!?\n", &context); // Разделение строки на предложения
 
-        While (sentence != NULL) {
+        while (sentence != NULL) {
             // Удаление начальных и конечных пробелов
-            While (iswspace(*sentence)) {
-                Sentence++;
+            while (iswspace(*sentence)) {
+                sentence++;
             }
-            Wchar_t* end = sentence + wcslen(sentence) – 1;
-            While (end > sentence && iswspace(*end)) {
+            wchar_t* end = sentence + wcslen(sentence) - 1;
+            while (end > sentence && iswspace(*end)) {
                 *end = L'\0';
-                End--;
+                end--;
             }
 
             // Игнорирование пустых строк
-            If (wcslen(sentence) > 1 && isPalindrome(sentence)) {
-                Wprintf(L»%ls\n», sentence);
+            if (wcslen(sentence) > 1 && isPalindrome(sentence)) {
+                wprintf(L"%ls\n", sentence);
                 totalPalindromes++;
             }
 
-            Sentence = wcstok(NULL, L».!?\n», &context);
+            sentence = wcstok(NULL, L".!?\n", &context);
         }
     }
 
-    Wprintf(L»\nОбщее количество палиндромических предложений: %d\n», totalPalindromes);
+    wprintf(L"\nОбщее количество палиндромических предложений: %d\n", totalPalindromes);
 
-    Fclose(file);
-    Free(line); // Освобождение динамически выделенной памяти
+    fclose(file);
+    free(line); // Освобождение динамически выделенной памяти
 }
