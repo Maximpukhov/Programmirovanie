a8067481425dc1eabf64f71eb87351096b3b0580
Maximpukhov <124022126+Maximpukhov@users.noreply.github.com>
2023-05-21 12:00:32 +0700

    Update main.c

--- course work/src/main.c
+++ course work/src/main.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <wchar.h>
+#include <locale.h>
+#include <stdlib.h>
+#include "palindrome.h"
 
+int main(int argc, char ** argv) {
+    setlocale(LC_ALL, "");
+    if (argc != 2) {
+        wprintf(L "Использование:\n%s <имя файла>\n", argv[0]);
+        return -1;
+    }
+    checkPalindromesFromFile(argv[1]);
+
+    return 0;
+}
