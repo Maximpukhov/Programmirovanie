bd8e7976be09d3d44c819ca07f724c74096cdd51
Maximpukhov <124022126+Maximpukhov@users.noreply.github.com>
2023-05-21 16:13:07 +0700

    Update main.c

--- lab4/src/main.c
+++ lab4/src/main.c
@@ -2,14 +2,14 @@
 
 int main(void){
 	printf("Cyg2win - convert Cygwin-style Windows paths to original format\n");
-    char delim[MAX_DELIM_SIZE+3]; // 64 symbols, 1 oversize canary, 1 for endline, 1 for dirty \n hack 
+    char delim[MAX_DELIM_SIZE+3];
     printf("delim: ");
     if (input(delim, MAX_DELIM_SIZE) == -1) {
 		printf("Invalid input -- delimeter must be no longer than %d symbols.\n", MAX_DELIM_SIZE);
 		return -1;
 	}
     
-    char pathstr[MAX_PATHSTR_SIZE+3]; // same but 2048 instead of 64...
+    char pathstr[MAX_PATHSTR_SIZE+3];
     printf("paths: ");
     if (input(pathstr, MAX_PATHSTR_SIZE) == -1) {
 		printf("Invalid input -- paths string must be no longer than %d symbols.\n", MAX_PATHSTR_SIZE);
