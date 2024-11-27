4d79fa3f8f04fbc0447cffe54f64d1d138a867d2
Maximpukhov <124022126+Maximpukhov@users.noreply.github.com>
2024-11-27 16:46:29 +0700

    Create flights_db.pl

---
+++ FLP/flights_db.pl
@@ -0,0 +1,5 @@
+flight(101, 'Moscow', '12:00', 5000).
+flight(102, 'London', '14:00', 7000).
+flight(103, 'Paris', '16:30', 6000).
+flight(104, 'Moscow', '18:45', 4000).
+flight(105, 'New York', '22:00', 10000).
